e9c447836bf7c078ab1174f6918dbebd538ffaf8
Wei-Yi Chiu <bird1204@gmail.com>
2015-06-17 15:40:01 +0800

    bug fixed
    if @options[:option_name].nil?  to if @options[option_name].nil?

--- examples/server.rb
+++ examples/server.rb
@@ -6,8 +6,9 @@ require 'allpay'
 
 get '/' do
   client = Allpay::Client.new(mode: :test)
+  trade_no = "YUGUIYFI312312"
   @params = client.generate_checkout_params({
-    :MerchantTradeNo => SecureRandom.hex(4),
+    :MerchantTradeNo => trade_no,
     :TotalAmount => 1000,
     :TradeDesc => "tradedesc",
     :ItemName => "itemname超強的",
@@ -15,7 +16,7 @@ get '/' do
     :ClientBackURL => "http://localhost:4567/",
     :ChoosePayment => 'Credit',
     :InvoiceMark => "Y",
-    :RelateNumber => SecureRandom.hex(4),
+    :RelateNumber => trade_no,
     :CustomerID => "",
     :CustomerIdentifier => "",
     :CustomerName => "屁孩章嘉",
@@ -38,5 +39,37 @@ get '/' do
     :DelayDay => "0",
     :InvType => "05"
   })
+  @str = client.gen_check_mac_value({
+    :MerchantTradeNo => trade_no,
+    :TotalAmount => 1000,
+    :TradeDesc => "tradedesc",
+    :ItemName => "itemname超強的",
+    :ReturnURL => "http://localhost:4567/",
+    :ClientBackURL => "http://localhost:4567/",
+    :ChoosePayment => 'Credit',
+    :InvoiceMark => "Y",
+    :RelateNumber => trade_no,
+    :CustomerID => "",
+    :CustomerIdentifier => "",
+    :CustomerName => "屁孩章嘉",
+    :CustomerAddr => "",
+    :CustomerPhone => "",
+    :CustomerEmail => "屁孩章嘉ghawhicomte",
+    :ClearanceMark => "",
+    :TaxType => "1",
+    :CarruerType => "",
+    :CarruerNum => "",
+    :Donation => "2",
+    :LoveCode => "",
+    :Print => "0",
+    :InvoiceItemName => "itemname超強的",
+    :InvoiceItemCount => "1",
+    :InvoiceItemWord => "個",
+    :InvoiceItemPrice => 1000,
+    :InvoiceItemTaxType => "1",
+    :InvoiceRemark => "1",
+    :DelayDay => "0",
+    :InvType => "05"
+    })
   erb :index
 end
