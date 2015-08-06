62f0dbd28b2f9a27bf26acfc62a1f113f9622e91
Wei-Yi Chiu <bird1204@gmail.com>
2015-08-06 16:52:49 +0800

    ...

--- examples/server.rb
+++ examples/server.rb
@@ -8,37 +8,42 @@ get '/' do
   client = Allpay::Client.new(mode: :test)
   trade_no = SecureRandom.hex(6)
   @params = client.generate_checkout_params({
-    :MerchantTradeNo => trade_no,
-    :TotalAmount => 1000,
-    :TradeDesc => "tradedesc",
-    :ItemName => "屁孩章嘉的屁孩哈特佛",
-    :ReturnURL => "http://localhost:4567/",
-    :ClientBackURL => "http://localhost:4567/",
-    :ChoosePayment => 'Credit',
-    :InvoiceMark => "Y",
-    :RelateNumber => trade_no,
-    :CustomerID => "",
-    :CustomerIdentifier => "",
-    :CustomerName => "屁孩華夏",
-    :CustomerAddr => "",
-    :CustomerPhone => "",
-    :CustomerEmail => "bird.chiu@sun-innovation.com",
-    :ClearanceMark => "",
-    :TaxType => "1",
-    :CarruerType => "",
-    :CarruerNum => "",
-    :Donation => "2",
-    :LoveCode => "",
-    :Print => "0",
-    :InvoiceItemName => "屁孩章嘉的屁孩哈特佛",
-    :InvoiceItemCount => "1",
-    :InvoiceItemWord => "個",
-    :InvoiceItemPrice => 1000,
-    :InvoiceItemTaxType => "1",
-    :InvoiceRemark => "1",
-    :DelayDay => "0",
-    :InvType => "05"
+                                              :MerchantTradeNo => trade_no,
+                                              :TotalAmount => 123123,
+                                              :TradeDesc => "屁孩章嘉的屁孩哈特佛",
+                                              :ItemName => "屁孩章嘉的屁孩哈特佛",
+                                              :ReturnURL => 'http://localhost:4567/',
+                                              :ClientBackURL => 'http://localhost:4567/',
+                                              :ChoosePayment => 'ALL',
+                                              :InvoiceMark => 'Y',
+                                              :RelateNumber => trade_no,
+                                              :CustomerID => '',
+                                              :CustomerIdentifier => '',
+                                              :CustomerName => '屁孩章嘉的屁孩哈特佛',
+                                              :CustomerAddr => '',
+                                              :CustomerPhone => '',
+                                              :CustomerEmail => 'fewfew@iofjoa.com',
+                                              :ClearanceMark => '',
+                                              :TaxType => '1',
+                                              :CarruerType => '',
+                                              :CarruerNum => '',
+                                              :Donation => '2',
+                                              :LoveCode => '',
+                                              :Print => '0',
+                                              :InvoiceItemName => '屁孩章嘉的屁孩哈特佛',
+                                              :InvoiceItemCount => '1',
+                                              :InvoiceItemWord => 'piece',
+                                              :InvoiceItemPrice => 123123,
+                                              :InvoiceItemTaxType => '1',
+                                              :InvoiceRemark => '1',
+                                              :DelayDay => '0',
+                                              :InvType => '05',
+                                              :PeriodAmount => 123123,      # 每次授權金額
+                     :PeriodType => 'M',                  # 週期種類
+                     :Frequency => 12,                      # 執行頻率(一年幾次)
+                     :ExecTimes => 99,                      # 總共執行幾次
+                     :PeriodReturnURL => 'http://localhost:4567/'
   })
-
-  erb :index
+  p "#{client.verify_mac(@params)}"
+    erb :index
 end
--- lib/allpay/client.rb
+++ lib/allpay/client.rb
@@ -43,9 +43,7 @@ module Allpay
       raw = pre_encode(params).sort_by{|x| x.to_s.downcase}.map!{|k,v| "#{k}=#{v}"}.join('&')
       padded = "HashKey=#{@options[:hash_key]}&#{raw}&HashIV=#{@options[:hash_iv]}"
       url_encoded = url_encode(padded).downcase!
-      mac = Digest::MD5.hexdigest(url_encoded).upcase!
-      p mac
-      mac
+      Digest::MD5.hexdigest(url_encoded).upcase!
     end
 
     #base from CGI::escape
@@ -62,13 +60,14 @@ module Allpay
     def verify_mac params = {}
       stringified_keys = params.stringify_keys
       check_mac_value = stringified_keys.delete('CheckMacValue')
-      p check_mac_value
-      p make_mac(stringified_keys) == check_mac_value
+      p "傳來的params #{stringified_keys}"
+      p "傳來的mac #{check_mac_value}"
+      p "驗證的mac #{make_mac(stringified_keys)}"
       make_mac(stringified_keys) == check_mac_value
     end
 
     def generate_params overwrite_params = {}
-      result = overwrite_params
+      result = overwrite_params.clone
       result[:MerchantID] = @options[:merchant_id]
       result[:CheckMacValue] = make_mac(result)
       result
